e6b2ebcff4b2a710a6310cb8ae550e276c25c474
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-09-14 16:40:53 -0300

    feat (products/routes): busca os produtos na database

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -6,12 +6,13 @@ async function getProducts(req, res) {
     const session = res.locals.session;
 
     try {
-        lista = await db.collection("produtos").find().toArray();
+        const products = await db.collection("produtos").find().toArray();
+
+        return res.status(STATUS_CODE.OK).send(products);
     } catch (error) {
-        console.log("ui");
+        console.error(error.message);
+        return res.sendStatus(STATUS_CODE.BAD_REQUEST);
     }
-    console.log(lista);
-    res.send(lista);
 }
 
 export { getProducts };
