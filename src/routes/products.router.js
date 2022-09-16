7c12eda6772e4e8d731b202d6af783a5dbe721ea
Francisco Carmo <franciscoacarmo@gmail.com>
2022-09-16 17:36:22 -0300

    Adiciona delete produto

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,17 +2,22 @@ import express from "express";
 import verifyAuth from "../middlewares/auth.middleware.js";
 import verifyCheckout from "../middlewares/checkout.middleware.js";
 import {
-    checkout,
-    getCart,
-    getProducts,
-    insertOnCart,
+  checkout,
+  getCart,
+  getProducts,
+  insertOnCart,
+  deleteOnCart,
 } from "../controllers/productsController.js";
+
+import { produtos } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.get("/products", verifyAuth, getProducts);
 router.post("/cart", verifyAuth, insertOnCart);
 router.get("/cart", verifyAuth, getCart);
+router.delete("/cart", verifyAuth, deleteOnCart);
+router.post("/produtos", produtos);
 router.post("/checkout", verifyAuth, verifyCheckout, checkout);
 
 export default router;
