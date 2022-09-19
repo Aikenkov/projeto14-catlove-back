302c54115da72a51a9ac7d53e68fc52ec7e6bf97
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-09-18 21:50:46 -0300

    ajuste

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ server.use(express.json());
 server.use(productsRouter);
 server.use(userRouter);
 
+server.get("/status", (re, res) => {
+    return res.send("Ok");
+});
+
 server.listen(process.env.PORT, () => {
     console.log(`Listening on port: ${process.env.PORT}`);
 });
